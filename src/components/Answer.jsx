aba96b107f5413fdbe887f1e370d64e4b25de0c4
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-04 01:03:34 +0300

    Create Answer component

---
+++ src/components/Answer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Answer(){
+    return  <p>
+    Frontend Mentor offers realistic coding challenges to help developers improve their 
+  frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
+  all levels and ideal for portfolio building.
+  </p>
+}
