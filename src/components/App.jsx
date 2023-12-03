e0fb9368f500750304f290f4b11f93ac81c548c4
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-03 22:10:53 +0300

    Create Styles and components

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(){
+    return <div className="header">
+        <img src="./images/icon-star.svg" alt="page-icon-star"/>
+        <h1>FAQs</h1>
+
+    </div>
+}
