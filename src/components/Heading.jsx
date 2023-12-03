ce9826126f20a380f7f3bed445bf7bca8cfc0420
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-04 01:03:26 +0300

    Create Heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Heading(){
+    return <div className="header">
+    <img src="./images/icon-star.svg" alt="page-icon-star"/>
+    <h1>FAQs</h1>
+</div>
+}
