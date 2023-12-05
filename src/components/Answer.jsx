4e48499418d4ba2e727347d42d1cfbc78fe9c1c2
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-12-05 10:27:59 +0300

    Remove from layout

--- src/components/Answer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Answer(props){
-    return  <p style={{display : props.isOpen ? "none": "block"}}>
-    Frontend Mentor offers realistic coding challenges to help developers improve their 
-  frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-  all levels and ideal for portfolio building.
-  </p>
-}
