0ce909bc8b1590a5d0ff65bc5d6bd0e0f6ea046a
Amr Elsehety <amr.abdelazeem22@gmail.com>
2024-01-23 13:06:20 +0300

    Fix event handler for collapse and show

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Heading from "./Heading";
 import Question from "./Question";
 import QA from "../Q&A";
 
-export default function App(){
-    const [isClicked, setClicked] = useState(0);
+export default function App() {
+  const [isClicked, setClicked] = useState(0);
 
-    document.addEventListener("keydown", (event)=>{
-        if(event.key === 'Enter' || event.key === ' '){
-            event.target.click();
-        }
-    })
-    return <main className="container">
-    <Heading/>
-    {QA.map((section, index)=> (
-        <Question 
-        key={index}
-        id = {index}
-        question = {section.question}
-        isClicked = {isClicked === index}
-        onShow = {()=> setClicked(index)}
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.target.click();
+    }
+  });
+  return (
+    <main className="container">
+      <Heading />
+      {QA.map((section, index) => (
+        <Question
+          key={index}
+          id={index}
+          question={section.question}
+          isClicked={isClicked === index}
+          onShow={() => {
+            if (isClicked === index) {
+              setClicked(null);
+            } else {
+              setClicked(index);
+            }
+          }}
         />
-    ))}
+      ))}
     </main>
+  );
 }
